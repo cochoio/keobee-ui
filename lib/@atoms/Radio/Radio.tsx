fda2090888de0f617466b0f41a6e264b53eebb0c
ziponia <thtjwls@gmail.com>
2021-01-27 22:50:13 +0900

    fix KU-6, variable 변수 생성

---
+++ lib/@atoms/Radio/Radio.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { useUi } from "@lib/hooks"
+import cx from "classnames"
+
+import "./Radio.scss"
+
+export type RadioProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  variant?: "primary"
+  name?: string
+  label?: JSX.Element | string
+}
+
+export const Radio = React.forwardRef<HTMLInputElement, RadioProps>(
+  ({ className, label, ...props }, ref) => {
+    const { getPrefix } = useUi()
+
+    const prefix = getPrefix("radio")
+
+    const _className = cx(prefix, [], {})
+
+    return (
+      <label className={_className}>
+        <input type="radio" {...props} ref={ref} />
+        <span />
+        {label && <span>{label}</span>}
+      </label>
+    )
+  },
+)
---
+++ stories/Radio.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { Story, Meta } from "@storybook/react/types-6-0"
+import { Radio, RadioProps } from "@lib/@atoms/Radio"
+import { action } from "@storybook/addon-actions"
+
+export default {
+  title: "@atoms/Radio",
+  component: Radio,
+} as Meta
+
+const Template: Story<RadioProps> = (args) => {
+  return (
+    <>
+      <Radio onChange={action("change")} name="color" label="color-pink" />
+      <Radio onChange={action("change")} name="color" label="color-red" />
+      <hr />
+      <Radio onChange={action("change")} name="size" label="size-big" />
+      <Radio onChange={action("change")} name="size" label="size-small" />
+    </>
+  )
+}
+
+export const Primary = Template.bind({})
+
+Primary.args = {}
