f0cb17ff3910bae2447ae395c623a3b5c0b9324b
ziponia <thtjwls@gmail.com>
2021-01-28 23:37:00 +0900

    folder re setup, webpack

--- stories/Input.stories.tsx
+++ stories/Input.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Story, Meta } from "@storybook/react/types-6-0"
-import { Input, InputProps } from "@lib/@atoms/Input"
+import { Input, InputProps } from "@lib/atoms/Input"
 import { action } from "@storybook/addon-actions"
 
 export default {
