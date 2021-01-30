985464e803bb3768756af2902c8f64ca14e1cc4e
ziponia <thtjwls@gmail.com>
2021-01-30 20:44:18 +0900

    fix:prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,7 @@
-console.log(`Start Formatter`)
-
 module.exports = {
   bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: false,
   trailingComma: "all",
   semi: false,
-  parser: "",
 }
